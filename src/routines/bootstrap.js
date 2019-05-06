c27e5feb63250a20032f449a36695d9b3e730ae6
Gajus Kuizinas <gajus@gajus.com>
2019-05-06 11:57:29 +0300

    style: compartmentalize HTTP binding code

--- src/routines/bootstrap.js
+++ src/routines/bootstrap.js
@@ -12,6 +12,7 @@ import {
   UnexpectedStateError
 } from '../errors';
 import {
+  bindHttpMethod,
   isUrlMatchingNoProxy,
   parseProxyUrl
 } from '../utilities';
@@ -19,50 +20,6 @@ import {
 const log = Logger.child({
   namespace: 'bootstrap'
 });
-
-const bindHttpMethod = (originalMethod, agent) => {
-  // eslint-disable-next-line unicorn/prevent-abbreviations
-  return (...args) => {
-    let url;
-    let options;
-    let callback;
-
-    if (typeof args[0] === 'string') {
-      url = args[0];
-
-      if (typeof args[1] === 'function') {
-        options = {};
-        callback = args[1];
-      } else {
-        options = {
-          ...args[1]
-        };
-        callback = args[2];
-      }
-    } else {
-      options = {
-        ...args[0]
-      };
-      callback = args[1];
-    }
-
-    if (!options.agent) {
-      options.agent = agent;
-    }
-
-    // `request` module sets `agent` property to `http.globalAgent`/ `https.globalAgent` by default.
-    if (options.agent === http.globalAgent || options.agent === https.globalAgent) {
-      options.agent = agent;
-    }
-
-    if (url) {
-      // $FlowFixMe
-      return originalMethod(url, options, callback);
-    } else {
-      return originalMethod(options, callback);
-    }
-  };
-};
 
 export default () => {
   global.GLOBAL_AGENT = global.GLOBAL_AGENT || {};
