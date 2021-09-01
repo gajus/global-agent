57e4d6543d853818e4462540ef99aab17f03d03b
vipendra-mehra <83336660+vipendra-mehra@users.noreply.github.com>
2021-09-01 19:01:14 +0530

    Update types.ts
    Updated type of ca to either string | string[] | undefined

--- src/types.ts
+++ src/types.ts
@@ -18,7 +18,7 @@ export type ProxyConfigurationType = {
 };
 
 export type TlsConfigurationType = {
-  ca?: string[],
+  ca?: string | string[] | undefined,
   cert?: string,
   ciphers?: string,
   clientCertEngine?: string,
@@ -55,12 +55,12 @@ export type ProxyAgentConfigurationInputType = {
   environmentVariableNamespace?: string,
   forceGlobalAgent?: boolean,
   socketConnectionTimeout?: number,
-  ca?: string[],
+  ca?: string | string[] | undefined,
 };
 
 export type ProxyAgentConfigurationType = {
   environmentVariableNamespace: string,
   forceGlobalAgent: boolean,
   socketConnectionTimeout: number,
-  ca: string[],
+  ca: string | string[] | undefined,
 };
