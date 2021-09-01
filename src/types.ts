932aa76f05e7604e28a6879e3292a78bf575eda5
vipendra-mehra <83336660+vipendra-mehra@users.noreply.github.com>
2021-09-01 19:33:35 +0530

    Update types.ts

--- src/types.ts
+++ src/types.ts
@@ -18,7 +18,7 @@ export type ProxyConfigurationType = {
 };
 
 export type TlsConfigurationType = {
-  ca?: string | string[] | undefined,
+  ca?: string | string[],
   cert?: string,
   ciphers?: string,
   clientCertEngine?: string,
@@ -55,12 +55,12 @@ export type ProxyAgentConfigurationInputType = {
   environmentVariableNamespace?: string,
   forceGlobalAgent?: boolean,
   socketConnectionTimeout?: number,
-  ca?: string | string[] | undefined,
+  ca?: string | string[],
 };
 
 export type ProxyAgentConfigurationType = {
   environmentVariableNamespace: string,
   forceGlobalAgent: boolean,
   socketConnectionTimeout: number,
-  ca: string | string[] | undefined,
+  ca?: string | string[],
 };
